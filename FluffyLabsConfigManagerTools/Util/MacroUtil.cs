4e9e78e761a21bcc2e235e08a7f4ce079ce1ba34
Jack Read <jackpendarvesread@gmail.com>
2019-10-14 22:42:39 +0100

    Added MacroUtil

---
+++ FluffyLabsConfigManagerTools/Util/MacroUtil.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using FluffyLabsConfigManagerTools.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyLabsConfigManagerTools.Util
+{
+    public class MacroUtil
+    {
+        private readonly BaseUnityPlugin plugin;
+
+        public MacroUtil(BaseUnityPlugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public ConfigEntry<Macro> AddMacroConfig(string section, string key, string description, bool isAdvanced)
+        {
+            return plugin.Config.AddSetting<Macro>(                
+                section,                
+                key,               
+                new Macro
+                {
+                    KeyboardShortcut = new BepInEx.Configuration.KeyboardShortcut(UnityEngine.KeyCode.None),
+                    MacroString = "",
+                    RepeatNumber = 1
+                },
+                new ConfigDescription(
+                    description,    
+                    null,
+                    new ConfigurationManagerAttributes { IsAdvanced = isAdvanced }
+                    )
+                );
+        }
+    }
+}
