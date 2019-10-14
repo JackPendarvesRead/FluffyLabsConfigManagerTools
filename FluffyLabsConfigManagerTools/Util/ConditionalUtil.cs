89afdb48d922aa868b974a1991eff3f3a6f9a557
Jack Read <jackpendarvesread@gmail.com>
2019-10-14 19:13:24 +0100

    Added ConditionalUtil

---
+++ FluffyLabsConfigManagerTools/Util/ConditionalUtil.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using FluffyLabsConfigManagerTools.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyLabsConfigManagerTools.Util
+{
+    public class ConditionalUtil
+    {
+        private readonly BaseUnityPlugin plugin;
+
+        public ConditionalUtil(BaseUnityPlugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public ConfigEntry<Conditional<T>> AddConditionalConfig<T>(string section, string key, T defaultValue, bool defaultCondition, ConfigDescription description)
+            where T : struct, IConvertible
+        {
+            return plugin.Config
+                .AddSetting<Conditional<T>>(                
+                section,
+                key,
+                new Conditional<T> { Condition = defaultCondition, Value = defaultValue },
+                description);                
+        }
+    }
+}
