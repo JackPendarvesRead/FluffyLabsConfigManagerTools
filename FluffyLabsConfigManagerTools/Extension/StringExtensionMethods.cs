fb67e3b80212b33d0ca48b492d5cbf2dbc735c72
Jack Read <jackpendarvesread@gmail.com>
2019-10-21 13:40:49 +0100

    Deleted unused extension method

--- FluffyLabsConfigManagerTools/Extension/StringExtensionMethods.cs
+++ FluffyLabsConfigManagerTools/Extension/StringExtensionMethods.cs
@@ -7,18 +7,7 @@ using System.Threading.Tasks;
 namespace FluffyLabsConfigManagerTools.Extension
 {
     internal static class StringExtensionMethods
-    {
-        public static string AppendZeroIfDecimal(this string s)
-        {
-            if (s.EndsWith("."))
-            {                
-                return s + "0";
-            }
-            else
-            {
-                return s;
-            }
-        }
+    {       
         public static string AppendZero(this string s)
         {
             if (s.Contains("."))
